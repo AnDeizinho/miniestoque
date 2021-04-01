a846da0c66c61691421b9ac22118288b0f26dc63
andrey <queirozandrey5@gmail.com>
2021-04-01 17:31:46 -0400

    formulário de novo item

---
+++ MINIESTOQUE/frmNovoItem.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace MINIESTOQUE
+{
+    public partial class frmNovoItem : Form
+    {
+        public frmNovoItem()
+        {
+            InitializeComponent();
+        }
+    }
+}
